fccbe9ac0edf64304663bce4b774e37591e3c8ed
Aditya Mehra <aix.m@outlook.com>
2022-01-20 09:38:31 +1030

    use pypi for all packages available

--- scripts/92-pip-mycroft.sh
+++ scripts/92-pip-mycroft.sh
@@ -1,18 +1,18 @@
 #Install Json Database
 (echo "**** Install Json Database ****")
-(pip3 install git+https://github.com/HelloChatterbox/json_database)
+(pip3 install json_database)
 
 # Install & Update OVOS Utils
 (echo "**** Install OVOS Utils****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos_utils)
+(pip3 install ovos_utils==0.0.14a4.post1)
 
 # Install & Update OVOS Workshop
 (echo "**** Install OVOS Workshop****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-workshop)
+(pip3 install ovos-workshop==0.0.5a7)
 
 # Install & Update OVOS Local Backend
 (echo "**** Install OVOS Local Backend****")
-(pip3 install git+https://github.com/OpenVoiceOS/OVOS-local-backend)
+(pip3 install ovos-local-backend==0.1.1)
 
 # Install OVOS Notifications Service
 (echo "**** Installing Notifications Service ****")
@@ -20,23 +20,23 @@
 
 # Install OVOS Mimic2 Plugin
 (echo "**** Installing OVOS Mimic-2 Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic2)
+(pip3 install ovos-tts-plugin-mimic2==0.1.2)
 
 # Install OVOS Mimic Plugin
 (echo "**** Installing OVOS Mimic Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic)
+(pip3 install ovos-tts-plugin-mimic==0.2.3)
 
 # Install OVOS VLC Plugin
 (echo "**** Installing OVOS VLC Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-vlc-plugin)
+(pip3 install ovos-plugin-vlc==0.0.1a3)
 
 # Install OVOS OCP Audio Plugin
 (echo "**** Installing OVOS OCP Audio Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin)
+(pip3 install ovos-plugin-common-play==0.0.1a9)
 
 # Install OVOS OCP Audio Plugin
 (echo "**** Installing OVOS Pico TTS Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-pico)
+(pip3 install ovos-tts-plugin-pico==0.0.3)
 
 # Install OVOS OCP Audio Plugin
 (echo "**** Installing Neon Larynx TTS Plugin ****")
@@ -44,19 +44,19 @@
 
 # Install OVOS Precise Lite Plugin
 (echo "**** Installing OVOS Precise Lite Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite)
+(pip3 install ovos-ww-plugin-precise-lite==0.1.0)
 
 # Install OVOS Precise Lite Plugin
 (echo "**** Installing OVOS Pocketspinx Plugin ****")
-(pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-pocketsphinx)
+(pip3 install ovos-ww-plugin-pocketsphinx==0.1.1)
 
 # Install OVOS Core
 (echo "**** Installing OVOS CORE ****")
-(pip3 install https://github.com/OpenVoiceOS/ovos-core/archive/playground/0.0.2.zip#egg=ovos-core[all])
+(pip3 install ovos-core==0.0.2a2[all])
 
 # Install OVOS Plugin Manager
 (echo "**** Installing OVOS Plugin Manager ****")
-(pip3 install https://github.com/openvoiceos/ovos-plugin-manager/archive/dev.zip)
+(pip3 install ovos-plugin-manager==0.0.3a4)
 
 # Install Requests Cache
 (echo "**** Installing Request Cache ****")
